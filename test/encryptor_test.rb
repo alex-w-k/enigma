9e9e469efab0f4a19a7d5b1988ba0aa0258cbdbd
alex549us3 <alex549us3@gmail.com>
2017-03-24 12:01:36 -0600

    Initial commit

---
+++ test/encryptor_test.rb
@@ -0,0 +1,12 @@
+gem 'minitest', '~>5.2'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/encryptor.rb'
+
+class Encryptor < MiniTest::Test
+
+  def test_init
+    
+  end
+
+end
