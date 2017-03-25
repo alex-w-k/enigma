466c312c5b0f276ab02317041a2a0cb760762ade
alex549us3 <alex549us3@gmail.com>
2017-03-25 12:54:57 -0600

    added test files for all classes

---
+++ test/decryptor_test.rb
@@ -0,0 +1,12 @@
+gem 'minitest', '~>5.2'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/decryptor.rb'
+
+class DecryptorTest < MiniTest::Test
+
+  def test_init
+    
+  end
+
+end
