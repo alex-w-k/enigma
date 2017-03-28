02f67e92f691c3039187e0e69fb445c6e561dd1f
Carl Richmond <richmond.carl@outlook.com>
2017-03-28 15:21:08 -0600

    decryption date fixes

--- lib/keygen.rb
+++ lib/keygen.rb
@@ -10,14 +10,18 @@ class KeyGen
               :key_c,
               :key_d
 
-  def initialize
-
-    @key  = self.key_generator
+  def initialize(key = self.key_generator, date = self.time)
+    @key  = key
+    @date = date
   end
 
   def key_generator
     chars = *(0..9)
     (1..5).map {chars.sample}.join
+  end
+
+  def date_entry(date = self.time)
+    @date = date
   end
 
   def time
@@ -29,27 +33,27 @@ class KeyGen
   end
 
   def offset
-    offset = ((self.time.to_i) ** 2).to_s
+    offset = ((@date.to_i) ** 2).to_s
     offset.split(//).last(4).join
   end
 
   def key_a
-    key_a = key[0] + key[1]
+    key_a = @key[0..1]
     key_a.to_i + offset[0].to_i
   end
 
   def key_b
-    key_b = key[1] + key[2]
+    key_b = @key[1..2]
     key_b.to_i + offset[1].to_i
   end
 
   def key_c
-    key_c = key[2] + key[3]
+    key_c = @key[2..3]
     key_c.to_i + offset[2].to_i
   end
 
   def key_d
-    key_d = key[3] + key[4]
+    key_d = @key[3..4]
     key_d.to_i + offset[3].to_i
   end
 
