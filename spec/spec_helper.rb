aab7d8395544f22a5e314a08b0d55ff4b7c553a1
Alex Koch <alex549us3@gmail.com>
2017-10-26 11:44:35 -0600

    fixes style code violations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,6 @@ RSpec.configure do |config|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
-
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
@@ -84,7 +83,6 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 end
-
 require 'pry'
 require './lib/encryptor.rb'
 require './lib/keygen.rb'
