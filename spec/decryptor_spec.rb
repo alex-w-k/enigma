039ac69e2c808b0d6b9eb04fb8d21f6fb4ebd0dd
Alex Koch <alex549us3@gmail.com>
2017-10-27 11:53:12 -0600

    adds more tests in rspec

--- spec/decryptor_spec.rb
+++ spec/decryptor_spec.rb
@@ -10,4 +10,25 @@ describe Decryptor do
       expect(decrypt.class).to eq(Decryptor)
     end
   end
+
+  describe '#message' do
+    it 'retaiins the message' do
+      decrypt.message = 'test'
+      expect(decrypt.message).to eq('test')
+    end
+  end
+
+  describe '#decrypt' do
+    context 'when given a message, key, and date' do
+      it 'correctly decrypts' do
+        expected = 'this is a test ..end..'
+        actual   = decrypt.decrypt("😰😩😷😷W😪!_😝\\\"😩😯😵ime😦😼😨ej", '55654', '300317')
+        expect(actual).to eq(expected)
+      end
+    end
+
+    context 'when given a message and key' do
+
+    end
+  end
 end
